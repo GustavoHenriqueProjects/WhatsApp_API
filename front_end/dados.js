94b641725d76e1aa2e7ab3d2bc1fe08c239bd652
Gustavo <gustavo326projetos@gmail.com>
2023-03-19 18:56:20 -0300

    Iniciando integração com postman

---
+++ front_end/dados.js
@@ -0,0 +1,18 @@
+/**************************************************************************************
+ * Objetivo: Realizar o comsumo de funções que fornecem o nome dos usarios que utilizam
+ * o WhatsApp e envia os dados para o front.
+ * Data: 19/03/2023
+ * Autor: Gustavo Henrique
+***************************************************************************************/
+
+'use strict'
+
+const select = document.querySelector('#lista_usuario')
+const selectInicial = select.value
+
+select.addEventListener('change', function () {
+    const usuarioSelecionado = select.value
+    
+})
+
+
